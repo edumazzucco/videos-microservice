bea1a4bce9263e11abb4a8ab7122c49f20e1ba7e
edumazzucco <edumazzucco@hotmail.com>
2023-06-09 13:29:31 -0300

    feat: first methods of category entity

--- src/category/domain/entities/category.ts
+++ src/category/domain/entities/category.ts
@@ -1,6 +1,32 @@
 //TDD - Kent Beck
 // Tests: Fail -> Success -> Refactor
 
-export class Category {
-    constructor(public name: string) {}
+export type CategoryProps = {
+    name: string;
+    description?: string;
+    is_active?: boolean;
+    created_at?: Date;
 }
+
+export class Category {
+    constructor(public readonly props: CategoryProps) {}
+
+    get name() {
+        return this.props.name
+    }
+
+    get description() {
+        return this.props.description
+    }
+
+    get is_active() {
+        return this.props.is_active
+    }
+
+    get created_at() {
+        return this.props.created_at
+    }
+}
+
+const category = new Category({name: 'Movie'})
+
